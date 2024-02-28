ffc6a5e926f8f113c629f87fcb953fb98b2dc43a
Jon Rhoades <jon.rhoades@maxwell.app>
2024-02-28 17:42:48 -0600

    change gqroder

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import Home from './views/home'
 import Home1 from './views/home1'
 import NotFound from './views/not-found'
 import TeamMember from './views/TeamMember'
+import QrCode from './views/QrCode'
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
         <Route component={Home} exact path="/" />
         <Route component={Home1} exact path="/home1" />
         <Route component={TeamMember} exact path="/team/:name" />
+        <Route component={QrCode} exact path="/code/:name" />
         <Route component={NotFound} path="**" />
         <Redirect to="**" />
       </Switch>
