1c048291fa2b856add4161dc5f2e6220c149bbdb
Jon Rhoades <jon.rhoades@maxwell.app>
2024-04-02 10:39:44 -0500

    forms

--- src/data/forms.js
+++ src/data/forms.js
@@ -1,6 +1,15 @@
 const forms = {
     "webinar-sso": {
-        title: ['Webinar:', 'How Single Sign On will effect Healthcare'],
+        title: ['Webinar:', 'How patient Single Sign On will effect Healthcare'],
+        subtitle: ["Transform Your Patient Care: Discover the Power of Single Sign-On to Elevate Security and Streamline Experience", '',],
+        dates: ["May 20th", "11:00am CST"],
+        form: "https://us.bigin.online/valid/forms/webinar-sso",
+        formHeight: 500,
+        cardsTitle: "Intake Done Better",
+        cardsSubtitle: "Discover how Valid can revolutionize patient onboarding for your healthcare organization or practice.",
+    },
+    "webinar-sso": {
+        title: ['Webinar:', 'How patient Instant Check in will effect Healthcare'],
         subtitle: ["Transform Your Patient Care: Discover the Power of Single Sign-On to Elevate Security and Streamline Experience", '',],
         dates: ["May 20th", "11:00am CST"],
         form: "https://us.bigin.online/valid/forms/webinar-sso",
